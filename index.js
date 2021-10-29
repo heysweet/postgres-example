1ac550010d813295445169eb50e13b5448a74a0b
Andrew Sweet <andrewmsweet@gmail.com>
2021-10-29 11:48:16 -0400

    Add pageview logging

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+const sequelize = new Sequelize('postgres://username:password@localhost:5432/dbname');
 sequelize
   .authenticate()
   .then(() => {
@@ -15,49 +15,51 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
-const User = sequelize.define('user', {
-  // attributes
-  firstName: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: Sequelize.STRING
-    // allowNull defaults to true
-  }
+const Pageview = sequelize.define('pageview', {
+    // attributes
+    timeStamp: {
+      primaryKey: true,
+      type: 'TIMESTAMP',
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false
+    },
+    ipAddress: {
+      type: Sequelize.INET,
+      allowNull: false
+    },
+    page: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   },
   {
     // options
   }
 );
 
+function getIp(req) {
+  return req.headers['x-forwarded-for'] ||
+     req.socket.remoteAddress ||
+     null;
+}
+
+async function asyncTrackPageview(pageName, req, res) {
+  try {
+    const newPageview = new Pageview({page: pageName, ipAddress: getIp(req)});
+    await newPageview.save();
+    // Returns the new user that is created in the database
+    res.json({ pageview: newPageview });
+  } catch(error) {
+    console.error(error);
+  }
+}
+
 // Note: using `force: true` will drop the table if it already exists
 // Now the `users` table in the database corresponds to the model definition
-User.sync({ force: true }); 
-app.get('/', (req, res) => res.json({ message: 'Hello World' }));
-app.post('/user', async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    // Returns the new user that is created in the database
-    res.json({ user: newUser });
-  } catch(error) {
-    console.error(error);
-  }
-});
-
-app.get('/user/:userId', async (req, res) => {
-  const userId = req.params.userId
-  try {
-    const user = await User.findAll({
-      where: {
-        id: userId
-      }
-    });
-    res.json({ user });
-  } catch(error) {
-    console.error(error);
-  }
+Pageview.sync({ force: true }); 
+app.get('/', (req, res) => {
+  asyncTrackPageview('home', req, res);
+  res.json({ message: 'Hello World' })
 });
 
 app.listen(PORT, () => {
